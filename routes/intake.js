9340424581f486599804abcebdaee0630ad55ec8
Amethyst Bibby <amethystbibby@gmail.com>
2022-09-02 17:11:01 +1000

    Added email send on intake form submission with Postmark.

--- routes/intake.js
+++ routes/intake.js
@@ -2,10 +2,16 @@ const { request } = require('express')
 const express = require('express')
 const router = express.Router()
 
+const emailController = require('../controllers/email.js');
+
 // @desc    Intake Page
 // @route   GET /intake
 router.get('/', (req, res) => {
     res.render('intake.ejs')
 })
+
+// @desc    Send email on form submit
+// @route   PUT /sendEmail
+router.put('/sendEmail', emailController.sendEmail);
 
 module.exports = router;
