31af367548671bb09352c9cc13e3f53e16f0fa66
Amethyst Bibby <amethystbibby@gmail.com>
2022-09-04 20:26:35 +1000

    Added template model to sendEmail function

--- controllers/email.js
+++ controllers/email.js
@@ -9,10 +9,28 @@ module.exports = {
                 "From": "amethyst@amethystbibby.com",
                 "To": "amethyst@amethystbibby.com",
                 "Subject": "Medical Intake Form Completed",
-                // Will render ejs template showing answers instead of this
-                "HTMLBody": "<strong>Hello</strong>",
-                "TextBody": "Hello",
-                "MessageStream": "outbound"
+                "MessageStream": "outbound",
+                "TemplateAlias": 'formSubmission',
+                "TemplateModel": {
+                    // Replace patient with PatientInfo object fetched from database
+                    "patient": {
+                        "givenName": "givenName_Value",
+                        "familyName": "familyName_Value",
+                        "address": "address_Value",
+                        "city": "city_Value",
+                        "state": "state_Value",
+                        "zipCode": "zipCode_Value",
+                        "phoneNumber": "phoneNumber_Value",
+                        "emailAddress": "emailAddress_Value",
+                        "occupation": "occupation_Value",
+                        "emergencyContact": "emergencyContact_Value",
+                        "emergencyContactPhone": "emergencyContactPhone_Value",
+                        "emergencyContactRelationship": "emergencyContactRelationship_Value",
+                        "physician": "physician_Value",
+                        "physicianPhone": "physicianPhone_Value",
+                        "digitalSignature": "digitalSignature_Value"
+                    },
+                }
             });
 
             console.log("Email sent")
