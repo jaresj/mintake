a4e3d902c35c8a7fb7bb43808e5a6c6dd915ea67
Kayla Burns <kaylabri95@gmail.com>
2022-09-05 01:01:35 -0400

    Routing issues fixed with SubmitUserForm

--- routes/main.js
+++ routes/main.js
@@ -10,5 +10,8 @@ router.post('/login', authController.postLogin)
 router.get('/logout', authController.logout)
 router.get('/signup', authController.getSignup)
 router.post('/signup', authController.postSignup)
+router.get('/submitUserForm', authController.getSignup)
+router.post('/submitUserForm', authController.postSignup)
+
 
 module.exports = router;
