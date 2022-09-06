5d476a6fab40b4b0ec6ae4a5795b129d9537b90f
Nothing Remains <101616116+NothingRemains@users.noreply.github.com>
2022-09-06 14:22:54 -0600

    -Removed functions that were not being used -Added function for sending emails to users upon request

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,11 @@
-const submitButton = document.querySelector('#submit');
 const emailButton = document.querySelector('#email');
-
-submitButton.addEventListener('click', submitUserForm);
-emailButton.addEventListener('click', sendEmail);
-
-// Will need to re-add an event listener to the submit button for emails when ready.
-
-async function submitForm() {
-  try {
-    const response = await fetch('intake/submitIntake', {
-      method: 'post',
-      headers: { 'Content-type': 'application/json' },
-    });
-
-    const data = await response.json();
-    console.log(data);
-    location.reload();
-  } catch (err) {
-    console.log(err);
-  }
-}
 
 async function sendEmail() {
   try {
-    const response = await fetch('intake/sendEmail', {
-      method: 'put',
-      headers: { 'Content-type': 'application/json' },
-    });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-async function submitUserForm() {
-  try {
-    const response = await fetch('submitUserForm', {
+    const response = await fetch('confirmation/sendEmail', {
       method: 'post',
       headers: { 'Content-type': 'application/json' },
     });
-
     const data = await response.json();
     console.log(data);
     location.reload();
