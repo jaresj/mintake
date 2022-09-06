a0b2d6042695fc9a5cb385596514cac65a40e64b
Nothing Remains <101616116+NothingRemains@users.noreply.github.com>
2022-09-06 14:20:08 -0600

    Added confirmation.js route for confirmation page and email submission

---
+++ routes/confirmation.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+// const authController = require('../controllers/auth');
+const confirmationController = require('../controllers/confirmation');
+const emailController = require('../controllers/email')
+const { ensureAuth } = require('../middleware/auth');
+
+router.get('/', ensureAuth, confirmationController.getConfirmation)
+router.post('/sendEmail', ensureAuth, emailController.sendEmail)
+
+module.exports = router;
