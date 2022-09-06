970f3530f29e8806d581a93a75f9cbef6d364bd7
Nothing Remains <101616116+NothingRemains@users.noreply.github.com>
2022-09-06 14:20:58 -0600

    -Commented out the getUser function, which should probably be removed -Modified the sendEmail function to send out a form email when requested

--- controllers/email.js
+++ controllers/email.js
@@ -3,56 +3,36 @@ const User = require('../models/User');
 const PatientInfo = require('../models/PatientInfo');
 
 module.exports = {
-  getUser: async (req, res) => {
+  // getUser: async (req, res) => {
+  //   try {
+  //     console.log(req.user);
+  //     const userData = req.user;
+  //     const userForm = await PatientInfo.find({ userId: userData._id });
+  //     console.log(`Sent from in the getUser block`);
+  //     console.log(userData, userForm);
+  //     res.redirect('/intake');
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // },
+  sendEmail: (req, res) => {
     try {
-      console.log(req.user);
-      const userData = req.user;
-      const userForm = await PatientInfo.find({ userId: userData._id });
-      console.log(`Sent from in the getUser block`);
-      console.log(userData, userForm);
-      res.redirect('/intake');
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  sendEmail: async (req, res) => {
-    try {
-      // Should have code to receive info from the user and database from the getUser function above.  You can probably either run the function and save the variables, or probably better to just move the userData/userForm from that block into here and get rid of getUser.
-
-      // Something like:
-      // const userData = req.user
+      const userData = req.user
       // const userForm = await PatientRecord.find({userId: userData._id})
       let client = new postmark.ServerClient(process.env.POSTMARK_STRING);
 
       client.sendEmail({
-        From: 'amethyst@amethystbibby.com',
-        // "To": userForm.emailAddress,
-        To: 'patientintakeapplication@gmail.com',
-        Subject: 'Medical Intake Form Completed',
-        MessageStream: 'outbound',
-        TemplateAlias: 'formSubmission',
-        TemplateModel: {
-          patient: {
-            givenName: userForm.givenName,
-            familyName: userForm.familyName,
-            address: userForm.address,
-            city: userForm.city,
-            state: userForm.state,
-            zipCode: userForm.zipCode,
-            phoneNumber: userForm.phoneNumber,
-            emailAddress: userForm.emailAddress,
-            occupation: userForm.occupation,
-            emergencyContact: userForm.emergencyContact,
-            emergencyContactPhone: userForm.emergencyContactPhone,
-            emergencyContactRelationship: userForm.emergencyContactRelationship,
-            physician: userForm.physician,
-            physicianPhone: userForm.physicianPhone,
-            digitalSignature: userForm.digitalSignature,
-          },
-        },
+        "From": 'Mintake Coordination Team <amethyst@amethystbibby.com>',
+        "To": 'patientintakeapplication@gmail.com',
+        // Switch out "To" field to the following to send to actual users:
+        // "To": userData.email,
+        "Subject": 'Medical Intake Form Completed',
+        "MessageStream": 'outbound',
+        "HtmlBody": "<h3>Valued Patient,</h3><br><p><b>Thank you for submitting your intake form for your upcoming appointment.  Your responses have been recorded and will be sent to your doctor for review prior to your appointment.<br><br>Best Wishes,<br><br>The Mintake Team</b></p>"
       });
 
       console.log('Email sent');
+      res.redirect('/')
     } catch (err) {
       console.error(err);
       process.exit(1);
